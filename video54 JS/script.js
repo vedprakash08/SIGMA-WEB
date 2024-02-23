abb7b5c68b9773f630b2036534683c5e3b7129fe
vedprakash08 <ved9552231@gmail.com>
2024-02-23 23:03:13 +0530

    67

---
+++ video54 JS/script.js
@@ -0,0 +1,16 @@
+alert("Hello World")
+console.log("Code is running.")
+console.log("Code is also running..")
+console.log("Code is getting ready to run...")
+
+var a=prompt("Enter your Number")
+console.log("Your Number is "+ a)
+var isTrue=confirm("If you want to leave this page then your computer get blast")
+if(isTrue){
+    console.log("Your computer is going to blast")
+}
+else{
+    console.log("Your computer is save")
+}
+document.title("JavaScript")
+document.body.style.backgroundColor="red"<<endl;
