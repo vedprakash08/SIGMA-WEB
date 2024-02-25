4bd19175443c3a36fb841d24f74ca1a0e3b2b3bc
vedprakash08 <ved9552231@gmail.com>
2024-02-25 22:56:52 +0530

    d'

---
+++ video55/script.js
@@ -0,0 +1,98 @@
+console.log("Hello i am here")
+var a=6;
+var c="Heloo";
+console.log(typeof a, typeof c)
+let b=8;
+console.log(a ,b)
+{
+    var a=67;
+    let b=46;
+    console.log(a,b)
+}
+console.log(a,b)
+// type of null is object
+
+let obj= {
+    "name" : "vipin",
+    "job role": "teacher"
+}
+
+console.log(obj)
+obj.salary="100cr";
+console.log(obj)
+obj.salary="120cr";
+console.log(obj)
+
+let z=9;
+var y=8;
+var t;
+let l;
+console.log(l)
+console.log(t)
+{
+    var y=37;
+    const s=10;
+    let z=10;
+}
+const s=18;
+console.log(s)
+z=z+1;
+console.log(z)
+console.log(y)
+
+// This is wrong
+// var z=5;
+// let z=3;             
+// console.log(z)
+
+// console.log("Hey this is tutorial 55");
+
+// var a = 5;
+// // a = a + 1
+// let b = 6;
+// let c = "Harry";
+// let _a = "Shubham";
+// // var 55a = "Rohan"; // Not Allowed
+
+// // console.log(a +  b + 8);
+// // console.log(typeof a, typeof b, typeof c);
+// {
+//     // let a = 66;
+//     console.log(a)
+// }
+// console.log(a)
+// // const a1 = 6;
+// // a1 = a1 + 1; // Not Allowed because a1 is constant
+
+
+// let x = "Harry bhai";
+// let y = 22;
+// let z = 3.55;
+// const p = true;
+// let q = undefined;
+// let r = null;
+
+// console.log(x, y, z, p, q, r)
+// console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+// let o = {
+//     "name": "Harry",
+//     "job code": 5600,
+//     "is_handsome": true
+// }
+
+// console.log(o);
+// o.salary = "100crores";
+// console.log(o);
+// o.salary = "500crores";
+// console.log(o);
+
+
+
+
+//Exercise
+let n="vipn";
+n=n+90;
+console.log(n)
+console.log(typeof n)
