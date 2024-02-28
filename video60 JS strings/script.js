b12fad931b369404f46f98619d28ce89eb4323e5
vedprakash08 <ved9552231@gmail.com>
2024-02-28 22:03:35 +0530

    f

---
+++ video60 JS strings/script.js
@@ -0,0 +1,39 @@
+console.log("This is strings tutorial")
+let a = "Harry";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+// console.log(a[5]);
+
+console.log(a.length)
+
+let real_name = "Harry"
+let friend = "Rohan"
+console.log("His name is " + real_name + " and his friends name is " + friend)
+console.log(`His name is ${real_name} and his friends name is ${friend}`)
+
+let b = "ShivamSh"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase()) 
+console.log(b.length) 
+console.log(b.slice(1, 5)) 
+console.log(b.slice(1)) 
+
+console.log(b.replace("Sh", "77"))
+console.log(b.concat(a, "Aishwariya", "Rahul", "Priya"))
+
+console.log(b)
+
+let x='vippn';
+console.log(x.replace("p", "j"));
+
+console.log("this is \" ui")
+console.log("this is \n ui")
+console.log(x.startsWith("v"));
+console.log(x.startsWith("i"));
+console.log(x.endsWith('n'));
+
+let f="Please give Rs100";
+console.log(f.slice(14))
